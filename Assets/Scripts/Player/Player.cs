21e3a0d4d91ed2946ec0e5ddf83b1306804c4e2d
Nolan Lizmi <42084530+nlizmi@users.noreply.github.com>
2024-07-09 12:22:49 -0400

    More player controller changes using forces and drag

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,26 +14,72 @@ public class Player : MonoBehaviour, IPlayerActions
     private Vector2 aimAxes;
     private bool swing;
 
-    public float speed;
+    public float maxSpeed;
+    public float acceleration;
+    public float groundDrag;
+
     public float jumpForce;
+    public float airDrag;
+
+    private bool IsGrounded => Physics2D.Raycast(rb2D.transform.position, -rb2D.transform.up, rb2D.transform.localScale.y / 2 + 0.02f);
+    private bool IsChangingDirection => rb2D.velocity.x * moveAxis < 0;
     
-    private void Awake() {
+    private void Awake()
+    {
         controls = new Controls();
         controls.Player.AddCallbacks(this);
         controls.Player.Enable();
     }
 
-    private void FixedUpdate() {
-        rb2D.transform.position += rb2D.transform.right * moveAxis * speed * Time.deltaTime;
-        if (jump && IsGrounded()) {
+    private void FixedUpdate()
+    {
+        Move();
+        if (IsGrounded)
+        {
+            ApplyGroundDrag();
+            Jump();
+        }
+        else
+        {
+            ApplyAirDrag();
+        }
+    }
+
+    private void Move()
+    {
+        rb2D.AddForce(new Vector2(moveAxis, 0f) * acceleration);
+
+        if (Mathf.Abs(rb2D.velocity.x) > maxSpeed)
+        {
+            rb2D.velocity = new Vector2(Mathf.Sign(rb2D.velocity.x) * maxSpeed, rb2D.velocity.y);
+        }
+    }
+
+    private void ApplyGroundDrag()
+    {
+        if (moveAxis == 0f || IsChangingDirection)
+        {
+            rb2D.drag = groundDrag;
+        }
+        else
+        {
+            rb2D.drag = 0f;
+        }
+    }
+
+    private void ApplyAirDrag()
+    {
+        rb2D.drag = airDrag;
+    }
+
+    private void Jump()
+    {
+        if (jump)
+        {
+            ApplyAirDrag();
             rb2D.AddForce(rb2D.transform.up * jumpForce, ForceMode2D.Impulse);
             jump = false;
         }
-    }
-    
-    private bool IsGrounded()
-    {
-        return Physics2D.Raycast(rb2D.transform.position, -rb2D.transform.up, rb2D.transform.localScale.y / 2 + 0.02f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
