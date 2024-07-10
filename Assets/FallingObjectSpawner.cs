f88dedcf58cab191b270bb75f6ce65b166903c45
Emily <emily.silkin@gmail.com>
2024-07-10 10:22:18 -0700

    Created falling obstacle

---
+++ Assets/FallingObjectSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectSpawner : MonoBehaviour
+{
+    public GameObject fallingObjectPrefab; // Reference to the obstacle prefab
+    public float spawnInterval;  // Time between spawns
+    public Vector2 spawnArea = new Vector2(10, 0); // Area within which obstacles will be spawned
+
+    private float timer;
+
+    void Start()
+    {
+        spawnInterval = Random.Range(1, 10);
+        timer = spawnInterval;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            SpawnObstacle();
+            spawnInterval = Random.Range(0, 10);
+            timer = spawnInterval;
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        Vector2 spawnPosition = new Vector3(
+            Random.Range(-spawnArea.x / 2, spawnArea.x / 2),
+            transform.position.y
+        );
+
+        Instantiate(fallingObjectPrefab, spawnPosition, Quaternion.identity);
+    }
+
+}
