1c1337090f6c3fedfc7c8eade6e9b1466eb519c9
NerdyBlues <thesparkingofhope@gmail.com>
2024-07-11 14:47:40 -0400

    Play Test Levels
    Play test Levels

--- Assets/Scripts/FallingObstacleScript.cs
+++ Assets/Scripts/FallingObstacleScript.cs
@@ -6,7 +6,7 @@ public class FallingObstacleScript : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("ground") || collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("ground") || collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject); //play the animation!
         }
