1d941942042230b27fdc124698bedc2542ec3513
jujud4 <jmegad4@gmail.com>
2024-07-23 13:58:02 -0400

    Created Branch
    Test Level Branch

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -7,14 +7,19 @@ public class Wind : MonoBehaviour
     [SerializeField] private float windspeed = 2f;
     [SerializeField] private Vector2 direction;
     [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private Rigidbody2D rb2D2;
+
 
     private void Start()
     {
         rb2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        rb2D2 = GameObject.FindGameObjectWithTag("Obstacle").GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerStay2D(Collider2D collision)  
     {
         rb2D.AddForce(direction.normalized * windspeed, ForceMode2D.Impulse);
+        rb2D2.AddForce(direction.normalized * windspeed, ForceMode2D.Impulse);
     }
 }
+
