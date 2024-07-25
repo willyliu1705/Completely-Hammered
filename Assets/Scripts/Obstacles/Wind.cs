565029d68862856a2612631a79cec54367339058
jujud4 <jmegad4@gmail.com>
2024-07-25 16:28:25 -0400

    Wind Adjustment 2
    Re-adjusted the wind script and created a level that requires a switch press, wall jumps, and a moving platform to navigate

--- Assets/Scripts/Obstacles/Wind.cs
+++ Assets/Scripts/Obstacles/Wind.cs
@@ -7,19 +7,33 @@ public class Wind : MonoBehaviour
     [SerializeField] private float windspeed = 2f;
     [SerializeField] private Vector2 direction;
     [SerializeField] private Rigidbody2D rb2D;
-    [SerializeField] private Rigidbody2D rb2D2;
+    [SerializeField] private Rigidbody2D[] rb2D2;
+    public List<Rigidbody2D> rb2DList = new List<Rigidbody2D>();
 
-
-    private void Start()
+    private void FixedUpdate()
     {
-        rb2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        rb2D2 = GameObject.FindGameObjectWithTag("fallingObstacle").GetComponent<Rigidbody2D>();
+        foreach (Rigidbody2D rb2D in rb2DList)
+        {
+            rb2D.AddForce(direction.normalized * windspeed, ForceMode2D.Impulse);
+        }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)  
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        rb2D.AddForce(direction.normalized * windspeed, ForceMode2D.Impulse);
-        rb2D2.AddForce(direction.normalized * windspeed, ForceMode2D.Impulse);
+        Rigidbody2D rb2D = collision.GetComponent<Rigidbody2D>();
+        if (rb2D != null) 
+        { 
+            rb2DList.Add(rb2D);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Rigidbody2D rb2D = collision.GetComponent<Rigidbody2D>();
+        if (rb2D != null)
+        {
+            rb2DList.Remove(rb2D);
+        }
     }
 }
 
